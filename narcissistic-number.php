0a5c5ef714f679a711173a1ef60e713fedcee98a
Satriyo Budi <satriyobudiu@gmail.com>
2021-05-07 17:08:24 +0700

    finish

--- narcissistic-number.php
+++ narcissistic-number.php
@@ -1,42 +1,39 @@
 <?php
-// PHP program for checking of
-// Narcissistic number
 
-// Function to count digits
-function countDigit($n)
-{
-	if ($n == 0)
-		return 0;
-
-	return (1 + countDigit($n / 10));
-}
-
-// Returns true if n is
-// Narcissistic number
-function narcissistic( $n)
-{
-	// count the number of digits
-	$l = countDigit($n);
-	$dup = $n;
-	$sum = 0;
-
-	// calculates the sum of digits
-	// raised to power
-	while ($dup)
+	function countDigit($n)
 	{
-		$sum += pow($dup % 10, $l);
-		$dup = (int)$dup / 10;
+		if ($n == 0)
+			return 0;
+	
+		return (1 + countDigit($n / 10));
 	}
-
-	return ($n == $sum);
-}
-
-// Driver Code
-$n = 1634;
-if (narcissistic(!$n))
-	echo "true";
-else
-	echo "false";
-
-// This code is contributed by akt_mit
+	
+	
+	
+	function narcissistic( $n)
+	{
+		
+		$l = countDigit($n);
+		$dup = $n;
+		$sum = 0;s
+	
+		
+		
+		while ($dup)
+		{
+			$sum += pow($dup % 10, $l);
+			$dup = (int)$dup / 10;
+		}
+	
+		return ($n == $sum);
+	}
+	
+	
+	$n = 1634;
+	if (narcissistic(!$n))
+		echo "true";
+	else
+		echo "false";
+	
+	
 ?>
